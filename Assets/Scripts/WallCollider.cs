903a24eae5fa5d281e6e652a6ea99ba4683f09de
LAPTOP-2QRIF7JH\Manas Gangwani <manasjg1969@gmail.com>
2017-07-03 14:03:30 +0530

    Prototype Ready

--- Assets/Scripts/WallCollider.cs
+++ Assets/Scripts/WallCollider.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class WallCollider : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+    
+    GameObject Ball;
+    Vector3 StartPos;
+    int timer = 0;
+    // Use this for initialization
+    void Start()
+    {
+        Ball = GameObject.Find("Sphere");
+        StartPos = Ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer == 5)
+        {
+            Ball.transform.position = StartPos;
+            timer = 0;
+            CancelInvoke("timerIncrement");
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            collision.gameObject.transform.position = StartPos;
+            collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            collision.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            InvokeRepeating("timerIncrement", 5, 1);
+        }
+    }
+    void timerIncrement()
+    {
+        timer++;
+           
+    }
 }
---
+++ Assets/Scripts/WallTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTrigger : MonoBehaviour {
+
+  
+    GameObject Ball;
+    Vector3 StartPos;
+    // Use this for initialization
+    void Start()
+    {
+        Ball = GameObject.Find("Sphere");
+        StartPos = Ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("SPhere Detected");
+            other.gameObject.transform.position = StartPos;
+            other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            other.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            
+        }
+    }
+}
